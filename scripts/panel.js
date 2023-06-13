e8502fccbb5ea1c094d5b5926f76d0388e1e6bfc
denis-skripnik <scadens@yandex.ru>
2023-06-13 16:10:08 +0700

    Add Polygon zkEVM

--- scripts/panel.js
+++ scripts/panel.js
@@ -3,7 +3,8 @@ const contracts = {
 	"167005": "0x9257437b986b989EE791331a69Dfb7Bd1aEFeF21",
 	"84531": "0x389c942d852dd1831583101897f798608c16cf5e",
 	"534353": "0x3341C1e8Fd8c38D738AD984b9b4EEFc2e48277f5",
-	"59140": "0x516cBa9c3E2a0968748666CA858578c4E4BC839A"
+	"59140": "0x516cBa9c3E2a0968748666CA858578c4E4BC839A",
+	"1101": "0xc5076e7470e7bb1b16a84142f79f6fcba83fb9fd"
 }
 
 const explorers = {
@@ -11,7 +12,8 @@ const explorers = {
 	"167005": "https://explorer.test.taiko.xyz",
 	"84531": "https://goerli.basescan.org",
 	"534353": "https://blockscout.scroll.io",
-	"59140": "https://explorer.goerli.linea.build"
+	"59140": "https://explorer.goerli.linea.build",
+	"1101": "https://zkevm.polygonscan.com"
 }
 
 var chain_id = "3333";
